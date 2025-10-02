bbcc23c09621a55c0dbbfec21c0db4076356d7ab
zishan044 <winchesterfelix007@gmail.com>
2025-10-02 23:15:26 +0600

    applied versioning to models for parameter tuning

--- src/spaceflights/pipelines/data_processing/pipeline.py
+++ src/spaceflights/pipelines/data_processing/pipeline.py
@@ -1,24 +1,24 @@
-from kedro.pipeline import Pipeline, pipeline, node
+from kedro.pipeline import Pipeline, pipeline, node, Node
 from .nodes import preprocess_companies,preprocess_shuttles, create_model_input_table
 
 def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline([
-        (node(
+        Node(
             func=preprocess_companies,
             inputs='companies',
             outputs='preprocessed_companies',
             name='preprocess_companies_node',
-        )),
-        (node(
+        ),
+        Node(
             func=preprocess_shuttles,
             inputs='shuttles',
             outputs='preprocessed_shuttles',
             name='preprocess_shuttles_node',
-        )),
-        (node(
+        ),
+        Node(
             func=create_model_input_table,
             inputs=['preprocessed_companies', 'preprocessed_shuttles', 'reviews'],
             outputs='model_input_table',
             name='create_model_input_table_node'
-        )),
+        ),
     ])
--- src/spaceflights/pipelines/data_science/pipeline.py
+++ src/spaceflights/pipelines/data_science/pipeline.py
@@ -1,25 +1,43 @@
-from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, Node, pipeline
 from .nodes import split_data, train_model, evaluate_model
 
 
-def create_pipeline(**kwargs) -> Pipeline:
+def create_modeling_pipeline(**kwargs) -> Pipeline:
     return Pipeline([
-        (node(
+        Node(
             func=split_data,
             inputs=['model_input_table', 'params:model_options'],
             outputs=['X_train', 'X_test', 'y_train', 'y_test'],
             name='split_data_node',
-        )),
-        (node(
+        ),
+        Node(
             func=train_model,
             inputs=['X_train', 'y_train'],
             outputs='regressor',
             name='train_model_node',
-        )),
-        (node(
+        ),
+        Node(
             func=evaluate_model,
             inputs=['regressor', 'X_test', 'y_test'],
             outputs=None,
             name='evaluate_model_node',
-        )),
+        ),
     ])
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    modelling_pipeline = create_modeling_pipeline()
+    
+    active_pipeline = pipeline(
+        modelling_pipeline,
+        inputs={"model_input_table": "model_input_table"},
+        namespace="active_modeling_pipeline"
+    )
+    
+    candidate_pipeline = pipeline(
+        modelling_pipeline, 
+        inputs={"model_input_table": "model_input_table"},
+        namespace="candidate_modeling_pipeline"
+    )
+
+    return active_pipeline + candidate_pipeline
