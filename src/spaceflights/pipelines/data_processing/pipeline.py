a90f79a345ecd0f5dc24cbe37207db7143632bdc
zishan044 <winchesterfelix007@gmail.com>
2025-10-02 19:53:58 +0600

    implemented data processing pipeline

--- src/spaceflights/pipelines/data_processing/pipeline.py
+++ src/spaceflights/pipelines/data_processing/pipeline.py
@@ -1,10 +1,18 @@
-"""
-This is a boilerplate pipeline 'data_processing'
-generated using Kedro 1.0.0
-"""
-
-from kedro.pipeline import Node, Pipeline  # noqa
-
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import preprocess_companies,preprocess_shuttles
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    return Pipeline([
+        (node(
+            func=preprocess_companies,
+            inputs='companies',
+            outputs='preprocessed_companies',
+            name='preprocess_companies_node',
+        )),
+        (node(
+            func=preprocess_shuttles,
+            inputs='shuttles',
+            outputs='preprocessed_shuttles',
+            name='preprocess_shuttles_node',
+        )),
+    ])
